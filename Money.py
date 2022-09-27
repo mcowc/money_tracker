17ae14720977d8f32c06b0dfa111d4eee8a206d8
mcowc <112872961+mcowc@users.noreply.github.com>
2022-09-27 18:02:56 +1000

    Update Money.py

--- Money.py
+++ Money.py
@@ -6,25 +6,26 @@ def check_money():
 
     # Assigning vars from .txt file    
     with open('Money.txt', 'r') as file:
-        for ln, line in enumerate(file):
-            if ln == 0:
+        for n, line in enumerate(file):
+            if n == 0:
                 money = int(line)
             else:
                 x = line.split(", ", 1)
-                expenses.update({ln:[float((x)[0]), x[1]]})
+                expenses.update({n:[float((x)[0]), x[1]]})
 
     # Display our balance and expenses
     current_money = money
-    print ("You started with: $" + str(money))
-    print ("You have spent:")
+    
+    print ("You started with: $" + str(money) + "\nYou have spent:")
     for i in range(len(expenses)):
-        j = i+1
-        print(str(j) + ".  $" + str(expenses[j][0]))
-        current_money = current_money - expenses[j][0]
+        expense_n = i+1
+        print(str(expense_n) + ".  $" + str(expenses[expense_n][0]))
+        current_money = current_money - expenses[expense_n][0]
     print("You have $" + str(current_money) + " left")
 
     # Viewing expenses
     print("Please choose a number from 1 - " + str(len(expenses)) + " to see what the money was spent on, or type q to quit.\n")
+    
     while True:
         uinput = input()
         if uinput == 'q':
@@ -35,14 +36,13 @@ def check_money():
             except:
                 print("That is not a number. Please choose a number.")
             else:
-                gnum = False
+                got_num = False
                 for i in range(len(expenses)):
                     if int(uinput)-1 == i:
                         print(expenses[i+1][1])
-                        gnum = True
-                if gnum == False:
+                        got_num = True
+                if got_num == False:
                     print("Please choose a number within the range of 1 - " + str(len(expenses)))
-
 
 # Main Menu
 def main_menu():
